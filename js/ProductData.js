34f2e8c6fb58cba3206664a81df28f32725bd9bc
o6g7 <o6g7@ugrad.cs.ubc.ca>
2018-06-04 12:44:49 -0700

    commit3

--- js/ProductData.js
+++ js/ProductData.js
@@ -1,30 +1,30 @@
 module.exports = {
   // Load Mock Product Data Into localStorage
-  init: function() {
+  init: function () {
     localStorage.clear();
     localStorage.setItem('product', JSON.stringify([
       {
         id: '0011001',
         name: 'Cloak of Self-Esteem',
-        image: 'CloakOfSelgEstem.jpg',
-        description: 'Confident garb for your uncertain adventurer!',
+        image: 'CloakOfSelfEsteem.jpg',
+        description: 'Confident garb for your uncertain adventurer! Protects you against being charmed or frigthened',
         variants: [
           {
             sku: '123123',
-            type: 'Red',
+            type: 'Single!',
             price: 4.99,
             inventory: 1
 
           },
           {
             sku: '123124',
-            type: 'Romantic 2-Pack',
+            type: 'Romantic 2-Pack!',
             price: 12.99,
             inventory: 5
           },
           {
             sku: '1231235',
-            type: '6-pack for your whole party!',
+            type: '5-Pack for your whole party!',
             price: 19.99,
             inventory: 3
           }
@@ -34,3 +34,4 @@ module.exports = {
   }
 
 };
+
